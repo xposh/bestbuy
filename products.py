f5c4b1d2d57deb8e1538c043415c6556a8a00ac2
Posh <posh@Mac.fritz.box>
2026-01-19 10:11:10 +0100

    Finalize Product and Store classes

--- products.py
+++ products.py
@@ -75,15 +75,16 @@ class Product:
 
 # ---------- TEST CODE ----------
 
-bose = Product("Bose QuietComfort Earbuds", price=250, quantity=500)
-mac = Product("MacBook Air M2", price=1450, quantity=100)
+if __name__ == "__main__":
+    bose = Product("Bose QuietComfort Earbuds", price=250, quantity=500)
+    mac = Product("MacBook Air M2", price=1450, quantity=100)
 
-print(bose.buy(50))
-print(mac.buy(100))
-print(mac.is_active())
+    print(bose.buy(50))
+    print(mac.buy(100))
+    print(mac.is_active())
 
-bose.show()
-mac.show()
+    bose.show()
+    mac.show()
 
-bose.set_quantity(1000)
-bose.show()
+    bose.set_quantity(1000)
+    bose.show()
